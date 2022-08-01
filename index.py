67cc25bff53475fc1bb316f573223b347d2579b9
beltz18 <beltz.18kyodai@gmail.com>
2022-07-31 22:09:37 -0400

    added first routes and templates

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
-import flask
+from flask import Flask, render_template
+app = Flask(__name__)
 
-print("everything OK")
+users = []
+
+@app.route('/')
+def index():
+  return render_template('index.html', num_users=len(users), user=users)
+
+@app.route('/add_user/<string:user>/')
+def show_users(user):
+  users.append(user)
+  return f"Se ha agregado el usuario: {user}"
+
+# para correr la app:
+# export FLASK_APP="nombre_archivo.py" en Linux o set "FLASK_APP=nombre_archivo.py" en Windows
+# salir del entorno con deactivate y volver a entrar
+# correr el comando flask run
