a618be7a9354091fb868a2930808d2c3d251d596
beltz18 <beltz.18kyodai@gmail.com>
2022-08-04 11:04:04 -0400

    added d form to send POST and GET data

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 users = []
@@ -11,6 +11,25 @@ def index():
 def show_users(user):
   users.append(user)
   return f"Se ha agregado el usuario: {user}"
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  if request.method == 'POST':
+    name = request.form['name']
+    emai = request.form['email']
+    pasw = request.form['password']
+    user = {
+      "name": name,
+      "email": emai,
+      "password": pasw
+    }
+    users.append(user)
+
+    next = request.args.get('next', None)
+    if(next):
+      return redirect(next)
+    return redirect(url_for('index'))
+  return render_template('signup.html')
 
 # para correr la app:
 # export FLASK_APP="nombre_archivo.py" en Linux o set "FLASK_APP=nombre_archivo.py" en Windows
